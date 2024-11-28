ab4d6010fe22ecf6e89c0505645090d318896964
roellammerant <roellammerant@users.noreply.github.com>
2024-11-28 11:25:23 +0200

    Clean up code

--- CODE/Extra 2(Intra figure 2).R
+++ CODE/Extra 2(Intra figure 2).R
@@ -21,18 +21,11 @@ par(mar = c(3, 5, 2, 3))
 #                                                          #
 ##%######################################################%##
 
-barplot(as.numeric(c("10.08","129.01","-115.47","23.62")),
+barplot(as.numeric(c("10.05", "128.98", "-115.40", "23.64")),
         col = c("azure2","bisque","aquamarine","lightsalmon","azure2","bisque","aquamarine","lightsalmon"),
         ylab = "Explained variance of Height", space = as.numeric(c("0.25","0.25","0.25","0.25")),
-        cex.lab = 1.5, ylim=c(-175,175))
+        cex.lab = 1.5, ylim=c(-150,150))
 
-
-label=c("CWM")
-
-axis(1, 
-     at = seq(3 ,6 , 4), 
-     labels = label , 
-     tick=FALSE)
 
 legend("topright", legend = c("Species turnover", "Intraspecific variation", "Covariation", "Total variation"), 
        col=c("azure2","bisque","aquamarine","lightsalmon"),
@@ -44,17 +37,11 @@ legend("topright", legend = c("Species turnover", "Intraspecific variation", "Co
 #                                                          #
 ##%######################################################%##
 
-barplot(as.numeric(c("16.54", "20.18", "-5.15", "15.95")),
-        col = c("azure2","bisque","aquamarine","lightsalmon","azure2","bisque","aquamarine","lightsalmon"),
+barplot(as.numeric(c("16.55","50.88","-51.50","15.95")),
+        col = c("azure2","bisque","aquamarine","lightsalmon"),
         ylab = "Explained variance of Root depth", space = as.numeric(c("0.25","0.25","0.25","0.25")),
         cex.lab = 1.5, ylim=c(-60,60))
 
-label=c("CWM")
-
-axis(1, 
-     at = seq(3 ,6 , 4), 
-     labels = label , 
-     tick=FALSE)
 
 ##%######################################################%##
 #                                                          #
@@ -62,17 +49,10 @@ axis(1,
 #                                                          #
 ##%######################################################%##
 
-barplot(as.numeric(c("25.14", "1.96", "8.54", "35.64")),
-        col = c("azure2","bisque","aquamarine","lightsalmon","azure2","bisque","aquamarine","lightsalmon"),
+barplot(as.numeric(c("25.14","1.96","8.52","35.63")),
+        col = c("azure2","bisque","aquamarine","lightsalmon"),
         ylab = "Explained variance of R-S ratio", space = as.numeric(c("0.25","0.25","0.25","0.25")),
         cex.lab = 1.5, ylim=c(-60,60))
-
-label=c("CWM", "FDis")
-
-axis(1, 
-     at = seq(3 ,6 , 4), 
-     labels = label , 
-     tick=FALSE)
 
 
 ##%######################################################%##
@@ -81,17 +61,10 @@ axis(1,
 #                                                          #
 ##%######################################################%##
 
-barplot(as.numeric(c("12.37", "17.94", "-13.72", "16.58")),
-        col = c("azure2","bisque","aquamarine","lightsalmon","azure2","bisque","aquamarine","lightsalmon"),
+barplot(as.numeric(c("12.34","17.88","-13.60","16.62")),
+        col = c("azure2","bisque","aquamarine","lightsalmon"),
         ylab = "Explained variance of SLA", space = as.numeric(c("0.25","0.25","0.25","0.25")),
         cex.lab = 1.5, ylim=c(-60,60))
-
-label=c("CWM")
-
-axis(1, 
-     at = seq(3 ,6 , 4), 
-     labels = label , 
-     tick=FALSE)
 
 ##%######################################################%##
 #                                                          #
@@ -99,17 +72,10 @@ axis(1,
 #                                                          #
 ##%######################################################%##
 
-barplot(as.numeric(c("19.48", "12.88", "-14.94", "17.42")),
-        col = c("azure2","bisque","aquamarine","lightsalmon","azure2","bisque","aquamarine","lightsalmon"),
+barplot(as.numeric(c("19.51","12.82","-14.87","17.46")),
+        col = c("azure2","bisque","aquamarine","lightsalmon"),
         ylab = "Explained variance of PC1", space = as.numeric(c("0.25","0.25","0.25","0.25")),
         cex.lab = 1.5, ylim=c(-60,60))
-
-label=c("CWM")
-
-axis(1, 
-     at = seq(3 ,6 , 4), 
-     labels = label , 
-     tick=FALSE)
 
 ##%######################################################%##
 #                                                          #
@@ -117,17 +83,11 @@ axis(1,
 #                                                          #
 ##%######################################################%##
 
-barplot(as.numeric(c("18.57", "22.72", "0.00", "41.45")),
-        col = c("azure2","bisque","aquamarine","lightsalmon","azure2","bisque","aquamarine","lightsalmon"),
+barplot(as.numeric(c("39.72","22.66","-20.91","41.47")),
+        col = c("azure2","bisque","aquamarine","lightsalmon"),
         ylab = "Explained variance of PC2", space = as.numeric(c("0.25","0.25","0.25","0.25")),
         cex.lab = 1.5, ylim=c(-60,60))
 
-label=c("CWM")
 
-axis(1, 
-     at = seq(3 ,6 , 4), 
-     labels = label , 
-     tick=FALSE)
-
-dev.off ()
+dev.off()
 
